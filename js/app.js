6a82ea3d0d7a4055606fc39b8f232d2ebe8a19d4
Eddie <placenamehere1901@gmail.com>
2023-02-20 12:32:49 +0200

    changed navbar, sidebar and buttons

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
+const sidebarLink = document.querySelectorAll(".sidebar-btn-link");
+console.log(sidebarLink);
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 80) {
@@ -18,5 +20,12 @@ navBtn.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
 });
+
+sidebarLink.forEach((e) => {
+  e.addEventListener("click", () => {
+    console.log("hello from sidebarlink");
+    sidebar.classList.remove("show-sidebar");
+  });
+});
+
 // set year
-date.innerHTML = new Date().getFullYear();
